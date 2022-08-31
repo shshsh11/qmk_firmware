47274f461cb77ca9e58e74cc09868757e600a713
Shawn Gu <sdgu000@gmail.com>
2022-08-31 16:10:43 -0400

    adding id75 keymap

---
+++ keyboards/idobao/id75/keymaps/sdgu/rules.mk
@@ -0,0 +1,22 @@
+
+BOOTMAGIC_ENABLE = no     # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes            # USB Nkey Rollover
+STENO_ENABLE = yes
+COMBO_ENABLE = yes
+TAP_DANCE_ENABLE = no
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
+LAYOUTS = ortho_5x15
+
+LTO_ENABLE = yes
+CAPS_WORD_ENABLE = yes
