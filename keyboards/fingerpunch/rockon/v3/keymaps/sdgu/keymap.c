104697b2abaaf500685f9056b85add35f1732ca1
sandsq <sandsquaretinkerbrain@gmail.com>
2022-11-29 15:08:25 -0500

    reverse encoder direction

--- keyboards/fingerpunch/rockon/v3/keymaps/sdgu/keymap.c
+++ keyboards/fingerpunch/rockon/v3/keymaps/sdgu/keymap.c
@@ -113,44 +113,40 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     // default behavior if undefined
     if (index == 0) {
-        // Conditional to reverse the direction of encoder number 1
-        // The reason I have this is that for some of my boards, it supports two different types of encoders, and they may differ in direction
         #ifdef ENCODERS_A_REVERSE
         if (!clockwise) {
-          #else
-          if (clockwise) {
-          #endif
-            tap_code16(KC_WH_D);
-          }
-          else{
-            tap_code16(KC_WH_U);
-          }
+        #else
+        if (clockwise) {
+        #endif
+        tap_code16(KC_WH_D);
+        }
+        else{
+        tap_code16(KC_WH_U);
+        }
     }
     else if (index == 1) {
-      // Conditional to reverse the direction of encoder number 1
-      // The reason I have this is that for some of my boards, it supports two different types of encoders, and they may differ in direction
-      #ifdef ENCODERS_B_REVERSE
-      if (!clockwise) {
-      #else
-      if (clockwise) {
-      #endif
+        #ifdef ENCODERS_B_REVERSE
+        if (!clockwise) {
+        #else
+        if (clockwise) {
+        #endif
         tap_code16(KC_WH_D);
-      }
-      else{
+        }
+        else{
         tap_code16(KC_WH_U);
-      }
+        }
     }
     else if (index == 2) {
-      #ifdef ENCODERS_C_REVERSE
-      if (!clockwise) {
-      #else
-      if (clockwise) {
-      #endif
+        #ifdef ENCODERS_C_REVERSE
+        if (!clockwise) {
+        #else
+        if (clockwise) {
+        #endif
         tap_code16(KC_WH_D);
-      }
-      else{
+        }
+        else{
         tap_code16(KC_WH_U);
-      }
+        }
     }
 
     return true;
