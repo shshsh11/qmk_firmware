69b2356e1abb1d3c85c6fc7232623bec2b513ffb
sandsq <sandsquaretinkerbrain@gmail.com>
2023-09-24 12:51:30 -0400

    removing layer numbers

--- keyboards/fingerpunch/rockon/v3/keymaps/sdgu/keymap.c
+++ keyboards/fingerpunch/rockon/v3/keymaps/sdgu/keymap.c
@@ -1,5 +1,7 @@
 #include QMK_KEYBOARD_H
 
+
+// qmk compile -kb fingerpunch/rockon/v3 -km sdgu
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _MAIN,
@@ -76,8 +78,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT_rockon(
     _______,  KC_F1,      KC_F2,      KC_F3,      KC_F4,   KC_F5,   KC_F11,                             KC_F12,  KC_F6,         KC_F7,   KC_F8,    KC_F9,   KC_F10,   _______,
-    _______,  KC_1,       KC_2,       KC_3,       KC_4,    KC_5,    _______,                            _______, _______,       _______, _______,  _______, KC_PSCR,  KC_INS, 
-    _______,  KC_6,       KC_7,       KC_8,       KC_9,    KC_0,    _______,                            _______, MO(_SYMBOLS),  KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT, KC_DEL, 
+    _______,  _______,   _______,   _______,   _______,    _______,    _______,                            _______, _______,       _______, _______,  _______, KC_PSCR,  KC_INS, 
+    _______,  _______,   _______,   _______,   _______,    _______,    _______,                            _______, MO(_SYMBOLS),  KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT, KC_DEL, 
     _______,  KC_COLN,    KC_COMM,    KC_DOT,    _______,  KC_ENT, _______,                            _______, _______,        KC_HOME, KC_PGDN,  KC_PGUP, KC_END,   _______, 
               _______,    _______,    _______,    _______, _______, _______, _______, _______, _______, _______, KC_SPC,   _______, _______,  _______,  _______, 
                                                                                       _______
